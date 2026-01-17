1d67495f743c1905113f6b92dcffdbecd4d3b7f6
Alessandro <acunha.38@gmail.com>
2026-01-17 07:58:02 -0300

    adicionados metodos GET por email e por data

--- src/main/java/com/javanauta/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/javanauta/agendadortarefas/business/TarefasService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,5 +35,21 @@ public class TarefasService {
                 tarefasRepository.save(entity));
 
 
+    }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
+
+        return tarefaConverter.paraListaTarefasDTO(
+                tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal ));
+
+    }
+
+    public List<TarefasDTO> buscaTarefasPorEmail(String token) {
+
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+        List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
+
+        return tarefaConverter.paraListaTarefasDTO(listaTarefas);
     }
 }
